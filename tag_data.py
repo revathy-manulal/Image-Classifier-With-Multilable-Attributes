1881ad7d4fb701c300a6df7449ae9cd7830cedb2
Revathy Manulal <revathymanulal@hotmail.com>
2018-11-26 21:53:50 +0530

    Create tag_data.py

---
+++ tag_data.py
@@ -0,0 +1,117 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+
+class Tagging:
+
+    def __init__(self, dataset_path):
+
+        img_folder_name = "img"
+        eval_folder_name = "Eval"
+        anno_folder_name = "Anno"
+        list_eval_partition_file = "list_eval_partition.txt"
+        list_attr_img_file = "list_attr_img.txt"
+        list_category_img_file = "list_category_img.txt"
+
+        # Each element is a tuple of (image path, category, attributes)
+        self.train_imgs = []  
+        self.test_imgs = []   
+        self.val_imgs = []    
+
+        # Construct the paths
+        self.path = dataset_path
+        self.img_dir = os.path.join(self.path, img_folder_name)
+        self.eval_dir = os.path.join(self.path, eval_folder_name)
+        self.anno_dir = os.path.join(self.path, anno_folder_name)
+
+        self.list_eval_partition = os.path.join(self.eval_dir, list_eval_partition_file)
+        self.list_attr_img = os.path.join(self.anno_dir, list_attr_img_file)
+        self.list_category_img = os.path.join(self.anno_dir, list_category_img_file)
+
+        # Gather the Train, Test and Val image paths
+        self.read_img_files_list()
+
+    def read_img_files_list(self):
+
+        fashion_db = "fashion.db"
+        if os.path.exists(fashion_db):
+            
+            db = open(fashion_db, "rb")
+            self.train_imgs, self.val_imgs, self.test_imgs = pickle.load(db)
+            print("Training images", len(self.train_imgs))
+            print("Validation images", len(self.val_imgs))
+            print("Test images", len(self.test_imgs))
+            return
+
+        # Read in the image to category mapping
+        image_to_category = {}
+        with open(self.list_category_img) as f:
+            imgs_count = int(f.readline().strip())
+            _ = f.readline().strip()  # read and throw away the header
+
+            for line in f:
+                words = line.split()
+                image_to_category[words[0].strip()] = int(words[1].strip())
+
+        assert(imgs_count == len(image_to_category))
+
+
+        # Read in the image to attributes mapping
+        image_to_attributes = {}
+        with open(self.list_attr_img) as f:
+            imgs_count = int(f.readline().strip())
+            _ = f.readline().strip()  # read and throw away the header
+            for line in f:
+                words = line.split('jpg')
+                lst = [int(i) for i in words[1].strip().split()]
+                image_to_attributes[words[0].strip()+"jpg"] = lst
+        assert(imgs_count == len(image_to_attributes))
+
+
+        # flag=0
+
+        # Read in the images
+        with open(self.list_eval_partition) as f:
+            imgs_count = int(f.readline().strip())
+            _ = f.readline().strip()  # read and throw away the header
+
+            for line in f:
+                words = line.split()
+                img = words[0].strip()
+                category_idx = image_to_category[img]
+
+                
+
+                category = np.zeros(50)  # one hot encoded
+                category[category_idx - 1] = 1
+                attributes = np.asarray(image_to_attributes[img], dtype=np.int16)
+
+                # if flag==0:
+                #     print(img, image_to_attributes[img], category_idx, category, attributes)
+                #     flag +=1
+
+                for att in attributes:
+                    if attributes[att] ==-1:
+                        attributes[att] =0
+
+                if words[1].strip() == "train":
+                    self.train_imgs.append((img, category, attributes))
+                if words[1].strip() == "val":
+                    self.val_imgs.append((img, category, attributes))
+                if words[1].strip() == "test":
+                    self.test_imgs.append((img, category, attributes))
+
+        print("Training images", len(self.train_imgs))
+        print("Validation images", len(self.val_imgs))
+        print("Test images", len(self.test_imgs))
+        assert(imgs_count == (len(self.train_imgs)+len(self.test_imgs)+len(self.val_imgs)))
+
+        # Store the data structures
+        db = open(fashion_db, "wb")
+        pickle.dump((self.train_imgs, self.val_imgs, self.test_imgs), db)
+        db.close()
+        print("Data structures stored on filesystem as: ", fashion_db)
+
+
